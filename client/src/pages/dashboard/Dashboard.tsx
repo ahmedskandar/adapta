d3da3d3627b8f9a6635d0406ace630721854f49f
Ahmed <itsahmedlukman@gmail.com>
2023-01-11 16:39:32 +0300

    Added Chart Stats Feature

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import classes from "./Dashboard.module.css";
 import Form from "./Form";
 import ProtectedFeature from "./ProtectedFeature";
+import StatsCard from "./StatsCard";
 
 const Home: React.FC = () => {
   return (
     <>
-      <main>
+      <main className={classes.main}>
         <div className={classes.container}>
           <Form />
           <ProtectedFeature />
+          <section className={classes.statsContainer}>
+            <StatsCard />
+            <StatsCard />
+            <StatsCard />
+          </section>
         </div>
       </main>
     </>
---
+++ client/src/pages/dashboard/StatsCard.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import classes from "./StatsCard.module.css";
+import chart from "../../assets/svg/PieChart.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+
+const StatsCard: React.FC = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.chartCont}>
+        <img src={chart} className={classes.chart} alt="" />
+      </div>
+      <div className={classes.cont2}>
+        <div className={classes.sec1}>
+          <div>Impact</div>
+          <div className={classes.climateScore}>Climate Score</div>
+        </div>
+        <h2 className={classes.risk}>High risk</h2>
+        <div className={classes.sec2}>
+          <div className={classes.smallSec}>
+            <FontAwesomeIcon className={classes.redCircle} icon={faCircle} />
+            <div>
+              0 - 25% <br /> <b>High Risk</b>
+            </div>
+          </div>
+          <div className={classes.smallSec}>
+            <FontAwesomeIcon className={classes.yellowCircle} icon={faCircle} />
+            <div>
+              0 - 25% <br /> <b>Medium Risk</b>
+            </div>
+          </div>{" "}
+          <div className={classes.smallSec}>
+            <FontAwesomeIcon className={classes.greenCircle} icon={faCircle} />
+            <div>
+              0 - 25% <br /> <b>Low Risk</b>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatsCard;
