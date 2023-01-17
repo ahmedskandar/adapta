3ed850e567b8106097cfac102efb9943fb0196f2
Ahmed <itsahmedlukman@gmail.com>
2023-01-17 11:49:42 +0300

    Added map controls

--- client/src/pages/dashboard/Map.tsx
+++ client/src/pages/dashboard/Map.tsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
 import classes from "./Map.module.css";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, FeatureGroup, TileLayer, Marker, Popup, Circle } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw"
+
+
 
 const Map: React.FC = () => {
+
+  const _created = (e: any) => console.log(e);
+
+
   const useGeoLocation = () => {
     const [location, setLocation] = useState<any>({
       loaded: false,
@@ -44,6 +51,12 @@ const Map: React.FC = () => {
 
   const location2 = location.loaded && !location.error;
 
+
+  
+  if(location.error) {
+    return <div className={classes.container}>{location.error.message}</div>
+  }
+  
   if (!location2) {
     return <div className={classes.container}>Loading...</div>;
   }
@@ -57,8 +70,23 @@ const Map: React.FC = () => {
             : [location.coordinates.lat, location.coordinates.lng]
         }
         zoom={14}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
       >
+        <FeatureGroup>
+                <EditControl
+                  position="topright"
+                  onCreated={_created}
+                  draw={
+                    {
+                      /* rectangle: false,
+                    circle: false,
+                    circlemarker: false,
+                    marker: false,
+                    polyline: false, */
+                    }
+                  }
+                />
+              </FeatureGroup>
         <TileLayer
           attribution='&copy; <a href=https://www.protectedplanet.net/en">Protected planet</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
